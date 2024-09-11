34d33fd8dd3c090cc71df347a4284ba88e758b9d
temelkandaz <temelkandaz@gmail.com>
2024-09-11 13:54:12 +0300

    Initial commit

---
+++ brokage-firm-app/src/main/java/com/brokage/firm/brokage_firm_app/BrokageFirmAppApplication.java
@@ -0,0 +1,13 @@
+package com.brokage.firm.brokage_firm_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrokageFirmAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrokageFirmAppApplication.class, args);
+	}
+
+}
---
+++ brokage-firm-app/src/test/java/com/brokage/firm/brokage_firm_app/BrokageFirmAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brokage.firm.brokage_firm_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrokageFirmAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
